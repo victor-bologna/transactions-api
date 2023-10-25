65bf5a6edd2a7b7955dcad3a8deb26c0701c989f
victor-bologna <victorbologna@hotmail.com>
2023-10-25 15:11:41 -0300

    Creating Controllers.

---
+++ src/main/java/com/desafiopicpay/controllers/TransactionController.java
@@ -0,0 +1,25 @@
+package com.desafiopicpay.controllers;
+
+import com.desafiopicpay.domain.transaction.Transaction;
+import com.desafiopicpay.dtos.TransactionDTO;
+import com.desafiopicpay.services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transactions")
+public class TransactionController {
+    @Autowired
+    private TransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<Transaction> createTransaction(@RequestBody TransactionDTO transactionDTO) throws Exception {
+        Transaction transaction = this.transactionService.createTransaction(transactionDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(transaction);
+    }
+}
