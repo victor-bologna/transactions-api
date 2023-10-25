0291e8663adefbe6ea0d7e3f06d56c1db1415413
victor-bologna <victorbologna@hotmail.com>
2023-10-25 10:01:10 -0300

    Creating Transaction Repository

---
+++ src/main/java/com/desafiopicpay/repositories/TransactionRepository.java
@@ -0,0 +1,10 @@
+package com.desafiopicpay.repositories;
+
+import com.desafiopicpay.domain.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+}
